4355a72c80c6f8d4357d216eff598ae15008e71c
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-02 14:37:57 -0400

    display instances

--- src/display.java
+++ src/display.java
@@ -1,12 +1,15 @@
 
 import java.awt.Canvas;
 
+import javax.swing.JFrame;
+
 public class Display extends Canvas {
 
     int WIDTH = 800;
     int HEIGHT = 600;
 
     public static void main(String[] args) {
-        Display game = 
+        Display game = new Display();
+        JFrame frame = new JFrame();
     }
 }
