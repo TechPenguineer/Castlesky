b6c0584024b86ca8b66ed1350082f4d296fdcc59
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-02 14:43:26 -0400

    serialVersionUID

--- src/display.java
+++ src/display.java
@@ -5,6 +5,8 @@ import javax.swing.JFrame;
 
 public class Display extends Canvas {
 
+    private static final long serialVersionUID = 1L;
+
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
 
